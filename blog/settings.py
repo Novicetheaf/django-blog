f374ed746ebc36b644716d1c7d84ab970f84b036
Ed <edmundocall.eoc@gmail.com>
2020-04-10 10:49:22 +0000

    added heroku part 1

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#rzrs_i+f#m(@d2&^mei9_g_)d6v7xu6u$df$59=6il7zntaqr'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,13 +82,16 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "'#rzrs_i+f#m(@d2&^mei9_g_)d6v7xu6u$df$59=6il7zntaqr'")
+
+
+os.environ.setdefault("DATABASE_URL", "postgres://ucjidpflsbyqee:c269af2f55587e6f9ca3173c1113777ef079aaf6f125afd68d2d03cecf4d3170@ec2-54-247-79-178.eu-west-1.compute.amazonaws.com:5432/dmelqrcddkc9h")
