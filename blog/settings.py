7347268f43a142125b2c0e7e2ad2c9ee457e1795
Ed <edmundocall.eoc@gmail.com>
2020-04-10 12:14:46 +0000

    removed env

--- blog/settings.py
+++ blog/settings.py
@@ -89,14 +89,13 @@ if "DATABASE_URL" in os.environ:
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
-    print('Database URL not found. Using SQLite instead')
+    print("Postgres URL not found, using sqlite instead")
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
